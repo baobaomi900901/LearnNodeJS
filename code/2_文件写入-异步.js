01d123fdfed59d39e89ed9cc5fbf1ae7d1add4c0
MobyTang <baobaomi900901@icloud.com>
2023-11-12 13:59:17 +0800

    更新到 27 文件夹操作

--- code/2_文件写入-异步.js
+++ code/2_文件写入-异步.js
@@ -5,5 +5,5 @@
 
 // 1.引入fs模块
 const fs = require('fs'); // require是一个方法，用来加载模块的, fs是核心模块，直接引入即可
-fs.writeFileSync('./2_文件异步写入.text', '三人行，则必有我师焉-17')
+fs.writeFileSync('./2_文件写入-异步.text', '三人行，则必有我师焉-17')
 console.log(1 + 1);
